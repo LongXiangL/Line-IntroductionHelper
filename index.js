7b0a9d929450f9750452d3ef96cb360999cc4030
LongXiangL <bfh1104@gmail.com>
2023-06-23 12:05:22 +0800

    finished message handling

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const app = express()
+const linebot = require("linebot")//引用linebot SDK
+//辨識line channel的資訊
+const bot = linebot({
+  channelId: processing.env.channelId,
+  channelSecret: processing.env.channelSecret,
+  channelAccessToken: processing.env.channelAccessToken,
+})
+
+const linebotParser = bot.parser()
+//當有人傳完訊息給bot時
+bot.on("message",function(event){
+  // event.message.text是使用者傳給bot的訊息
+  const introRegex = /你|誰|介紹|you|yourself|hello|你好|hi/gi;
+  const resumeRegex = /resume|履歷|cv/gi;
+  const blogRegex = /blog|部落格|文章/gi;
+  const userText = event.message.text;
+  if (introRegex.test(userText)) {
+    event.reply(
+      "你好！我是龍驤，從體育老師到創業咖啡廳，跨營運生鮮食品，因為疫情接觸網頁開發與銷售，目前往全端/後端網頁工程師的領域邁進。"
+    );
+  } else if (resumeRegex.test(userText)) {
+    event.reply(
+      "以下是我目前的最新履歷以及檔案，正在尋找後端工程師職位，歡迎隨時與我聯繫 \n\n履歷:https://drive.google.com/file/d/1YLP6L4UyvBv7nDiJWwY1Nuq7SL4GUpTm/view?usp=sharing \n\nLinkedIn頁面: https://www.linkedin.com/in/%E9%BE%8D%E9%A9%A4-%E5%91%82-00042626a/\n\ngithub作品頁面:https://github.com/LongXiangL "
+    );
+  } else if (blogRegex.test(userText)) {
+    event.reply(
+      "以下是我的部落格連結，主要發布一些、專案筆記以及求職紀錄\n\n部落格連結: https://medium.com/@bfh1104"
+    );
+  } else {
+    event.reply(
+      "不知道該問什麼嗎？ 歡迎透過以下的關鍵字與我互動！\n\n介紹/履歷/部落格"
+    );
+  }
+});
+//送出帶有line-bot需要資訊的POST請求
+
+
+app.post("/", linebotParser) 
+
+
+app.listen(process.env.PORT||3000, () => {
+  console.log(`Example server star`);
+});
